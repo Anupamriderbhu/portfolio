941e25c9d9bd50c96a11f8034b568f8fd0a85f27
anupam <anupambhu65@gmail.com>
2024-05-07 00:07:54 +0530

    done

--- src/portfolio.js
+++ src/portfolio.js
@@ -27,7 +27,7 @@ const greeting = {
   ),
   resumeLink:
     "", // Set to empty to hide the button
-  // displayGreeting: true // Set false to hide this section, defaults to true
+  displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
